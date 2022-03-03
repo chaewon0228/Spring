cfa351e24a3b435482b868e73f12d3d467eddaaf
chaewon0228 <s2033@e-mirim.hs.kr>
2022-03-03 15:07:48 +0900

    Spring Container

---
+++ JavaBasic/src/kr/hs/study/beans/HelloWorldEn.java
@@ -0,0 +1,10 @@
+package kr.hs.study.beans;
+
+public class HelloWorldEn implements Hello{
+	public void sayHello(){
+		System.out.println("hello");
+	}
+	/*public static void main(String[] args) {
+		sayHello();
+	}*/
+}
