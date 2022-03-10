33237fb9f786272c3d99c5108271cc20169e125c
chaewon0228 <s2033@e-mirim.hs.kr>
2022-03-10 15:09:58 +0900

    Spring의 init/destroy method

---
+++ LifeCycle2/src/main/java/kr/hs/study/beans/DataBean1.java
@@ -0,0 +1,14 @@
+package kr.hs.study.beans;
+
+public class DataBean1 {
+	public DataBean1() {
+		System.out.println("DataBean1 기본 생성자");
+	}
+	public void init() {
+		System.out.println("init 호출");
+	
+	}
+	public void destory() {
+		System.out.println("destory 호출");
+	}
+}
