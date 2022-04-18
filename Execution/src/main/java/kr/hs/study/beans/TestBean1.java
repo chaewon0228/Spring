0db467b7b301cd601ad5a03a27fa561aa55c2b87
chaewon0228 <s2033@e-mirim.hs.kr>
2022-04-18 11:21:59 +0900

    aop:pointcut expression 속성

---
+++ Execution/src/main/java/kr/hs/study/beans/TestBean1.java
@@ -0,0 +1,24 @@
+package kr.hs.study.beans;
+
+public class TestBean1 {
+	public void method1() {
+		System.out.println("kr.hs.study.beans.TestBean1의 method1");
+	}
+
+	public void method1(int a1) {
+		System.out.println("int 매개변수 method1 : " + a1 );
+	}
+	
+	public void method1(int a1, int a2) {
+		System.out.println("int 매개변수 2개 method1 : " + a1 + ", " + a2);
+	}
+	
+	public void method1(String s1) {
+		System.out.println("String 매개변수 method1 : " + s1);
+	}
+	
+	
+	public void method2() {
+		System.out.println("kr.hs.study.beans.TestBean2의 method2");
+	}
+}
